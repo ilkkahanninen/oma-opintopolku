56c1f469eb321bb0e92b267ceb146809f297d02c
Wille Keltikangas <wille.keltikangas@gofore.com>
2018-05-21 12:30:15 +0300

    Add white versions of images

--- src/components/selection/Selection.js
+++ src/components/selection/Selection.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import SelectionItem from '../selectionitem/SelectionItem';
 import styles from './Selection.css';
-import koskiImg from 'Static/img/opintoni.svg';
-import omatsivutImg from 'Static/img/hakemukseni.svg';
+import koskiImg from 'Static/img/opintoni_white.svg';
+import omatsivutImg from 'Static/img/hakemukseni_white.svg';
 
 const Selection = () => (
   <div className={styles['flex-container']}>

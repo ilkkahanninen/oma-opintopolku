17d6c76f03ce789a8137fb6c211342c8e9e6a900
Wille Keltikangas <wille.keltikangas@gofore.com>
2018-05-23 11:28:05 +0300

    Fix language detection

--- src/i18n.js
+++ src/i18n.js
@@ -1,9 +1,11 @@
 import i18n from 'i18next';
 import Backend from 'i18next-xhr-backend';
 import { reactI18nextModule } from 'react-i18next';
+import LngDetector from 'i18next-browser-languagedetector';
 
 i18n
   .use(Backend)
+  .use(LngDetector)
   .use(reactI18nextModule)
   .init({
     fallbackLng: 'fi',
@@ -19,6 +21,10 @@ i18n
     },
     backend: {
       loadPath: '/oma-opintopolku/locales/{{lng}}/{{ns}}.json'
+    },
+    detection: {
+      order: ['cookie'],
+      lookupCookie: 'lang',
     }
   });
 

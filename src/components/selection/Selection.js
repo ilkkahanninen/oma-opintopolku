bf5e58389583e5ff05ec59750c635041b409d032
Heikki Hulkko <heikki.hulkko@reaktor.com>
2018-05-24 13:22:42 +0300

    /koski -> /opinnot

--- src/components/selection/Selection.js
+++ src/components/selection/Selection.js
@@ -7,7 +7,7 @@ import omatsivutImg from 'Static/img/hakemukseni_white.svg';
 const Selection = () => (
   <div className={styles['flex-container']}>
     <SelectionItem namespace='omatsivut' icon={omatsivutImg} link='/omatsivut' />
-    <SelectionItem namespace='koski' icon={koskiImg} link='/koski/omattiedot' />
+    <SelectionItem namespace='koski' icon={koskiImg} link='/opinnot/omattiedot' />
   </div>
 );
 

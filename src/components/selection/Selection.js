ff3b5786fa43d5f5d52951bc274f3f5822c97e6b
Petri Myllys <petri.myllys@reaktor.com>
2018-05-25 10:44:08 +0300

    Revert "/koski -> /opinnot"
    This reverts commit bf5e58389583e5ff05ec59750c635041b409d032.

--- src/components/selection/Selection.js
+++ src/components/selection/Selection.js
@@ -7,7 +7,7 @@ import omatsivutImg from 'Static/img/hakemukseni_white.svg';
 const Selection = ({isLoggedIn}) => (
   <div className={styles['flex-container']}>
     <SelectionItem isLoggedIn={isLoggedIn} namespace='omatsivut' icon={omatsivutImg} link='/omatsivut' />
-    <SelectionItem isLoggedIn={isLoggedIn} namespace='koski' icon={koskiImg} link='/opinnot/omattiedot' />
+    <SelectionItem isLoggedIn={isLoggedIn} namespace='koski' icon={koskiImg} link='/koski/omattiedot' />
   </div>
 );
 

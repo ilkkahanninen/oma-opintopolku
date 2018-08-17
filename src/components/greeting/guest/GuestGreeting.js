63f6ff202b61f982ccee5d87839ad752a65a7ae0
Petri Myllys <petri.myllys@reaktor.com>
2018-08-17 12:28:04 +0300

    Make login link clickable with keyboard

--- src/components/greeting/guest/GuestGreeting.js
+++ src/components/greeting/guest/GuestGreeting.js
@@ -3,12 +3,17 @@ import { I18n } from 'react-i18next';
 import { login } from '../../../utils.js'
 import styles from '../Greeting.css';
 
+const handleLoginClick = e => {
+  e.preventDefault()
+  login()
+}
+
 const GuestGreeting = () => (
   <I18n ns="home">
     {t => (
       <div className={styles['greeting-container-space-below']}>
         <p className={styles['guest-greeting']}>{t('login.info')}</p>
-        <a className={styles['login-button']} onClick={ login } tabIndex="0">{t('common:login')}</a>
+        <a href="" className={styles['login-button']} onClick={ handleLoginClick } tabIndex="0">{t('common:login')}</a>
       </div>
     )}
   </I18n>

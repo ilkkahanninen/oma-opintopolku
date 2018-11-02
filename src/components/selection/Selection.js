ec8c0f6d0c2fab10c6d6f10f6f34220a078ca4a8
Petri Myllys <petri.myllys@reaktor.com>
2018-11-02 10:54:50 +0200

    Lisää linkki Tietojeni käyttö -sivulle

--- src/components/selection/Selection.js
+++ src/components/selection/Selection.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import SelectionItem from '../selectionitem/SelectionItem';
+import Link from '../link/Link'
 import styles from './Selection.css';
 import koskiImg from 'Static/img/opintoni_white.svg';
 import omatsivutImg from 'Static/img/hakemukseni_white.svg';
+import tietojenikayttoImg from 'Static/img/tietojenikaytto.svg';
 
 const Selection = ({isLoggedIn}) => (
-  <div className={styles['flex-container']}>
-    <SelectionItem isLoggedIn={isLoggedIn} namespace='omatsivut' icon={omatsivutImg} link='/omatsivut' />
-    <SelectionItem isLoggedIn={isLoggedIn} namespace='koski' icon={koskiImg} link='/koski/omattiedot' />
-  </div>
+  <React.Fragment>
+    <div className={styles['flex-container']}>
+      <SelectionItem isLoggedIn={isLoggedIn} namespace='omatsivut' icon={omatsivutImg} link='/omatsivut' />
+      <SelectionItem isLoggedIn={isLoggedIn} namespace='koski' icon={koskiImg} link='/koski/omattiedot' />
+    </div>
+    <div className={styles['bottom-container']}>
+      <Link isLoggedIn={isLoggedIn} namespace='tietojenikaytto' icon={tietojenikayttoImg} link='/koski/omadata/kayttooikeudet' />
+    </div>
+  </React.Fragment>
 );
 
 export default Selection;

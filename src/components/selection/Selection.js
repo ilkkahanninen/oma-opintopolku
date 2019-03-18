90fa959ecb490d8f3246f9aeeec37330b91f473b
Jarkko Pätäri <jarkko.patari@gofore.com>
2019-03-18 13:39:49 +0200

    Add varda link

--- src/components/selection/Selection.js
+++ src/components/selection/Selection.js
@@ -3,6 +3,7 @@ import SelectionItem from '../selectionitem/SelectionItem';
 import Link from '../link/Link'
 import styles from './Selection.css';
 import koskiImg from 'Static/img/opintoni_white.svg';
+import vardaImg from 'Static/img/lukko_white.svg';
 import omatsivutImg from 'Static/img/hakemukseni_white.svg';
 import tietojenikayttoImg from 'Static/img/tietojenikaytto.svg';
 
@@ -11,6 +12,7 @@ const Selection = ({isLoggedIn}) => (
     <div className={styles['flex-container']}>
       <SelectionItem isLoggedIn={isLoggedIn} namespace='omatsivut' icon={omatsivutImg} link='/omatsivut' />
       <SelectionItem isLoggedIn={isLoggedIn} namespace='koski' icon={koskiImg} link='/koski/omattiedot' />
+      <SelectionItem isLoggedIn={isLoggedIn} namespace='varda' icon={vardaImg} link='/varda' />
     </div>
     <div className={styles['bottom-container']}>
       <Link isLoggedIn={isLoggedIn} namespace='tietojenikaytto' icon={tietojenikayttoImg} link='/koski/omadata/kayttooikeudet' />

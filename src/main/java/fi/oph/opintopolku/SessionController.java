cea2ed5cc68ee13dbb0f48ba7872e51896c2d480
Tatu Honkanen <tatu.honkanen@cybercom.com>
2020-09-24 08:33:23 +0300

    OY-410 : Session storage added

--- src/main/java/fi/oph/opintopolku/SessionController.java
+++ src/main/java/fi/oph/opintopolku/SessionController.java
@@ -1,26 +1,46 @@
 package fi.oph.opintopolku;
 
 import lombok.val;
+import org.jasig.cas.client.authentication.AttributePrincipal;
 import org.joda.time.LocalDate;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.cas.authentication.CasAssertionAuthenticationToken;
+import org.springframework.security.cas.authentication.CasAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
 
 @RestController
 @RequestMapping(value = "/session")
 public class SessionController {
-
+    @PreAuthorize("isAuthenticated()")
     @GetMapping
-    public User getSession(@RequestHeader(value = "nationalidentificationnumber", required = false) String hetu,
-                           @RequestHeader(value = "firstname", required = false) String etunimet,
-                           @RequestHeader(value = "sn", required = false) String sukunimi) {
-        val user = new User();
-        String displayName = ShibbolethUtils.parseDisplayName(etunimet, sukunimi);
-        user.setName(displayName);
+//    public User getSession(@RequestParam(value = "ticket", required = false) String ticket) {
+    public User getSession() {
 
-        LocalDate bd = ShibbolethUtils.parseDateFromHetu(hetu);
-        user.setBirthDay(bd);
+        //private static Optional<Authentication> getAuthentication() {
+        //    return Optional.ofNullable(SecurityContextHolder.getContext().getAuthentication());
+        //}
+        // TODO test start
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        CasAuthenticationToken casAuthenticationToken = (CasAuthenticationToken) authentication;
+        AttributePrincipal principal = casAuthenticationToken.getAssertion().getPrincipal();
+        Map attributes = principal.getAttributes();
+        String personOid = (String) attributes.getOrDefault("personOid", "NOT_FOUND");
+        //TODO test end
+
+        //Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        val user = new User();
+        //val userName = getAuthentication().flatMap(authentication -> Optional.ofNullable(authentication.getName()));
+        user.setName("testi testiajaja");
+        //String displayName = ShibbolethUtils.parseDisplayName(etunimet, sukunimi);
+        //user.setName(displayName);
+
+       // LocalDate bd = ShibbolethUtils.parseDateFromHetu(hetu);
+       // user.setBirthDay(bd);
 
         return user;
     }

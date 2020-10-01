f4c39fb9bfbace1d05fa78181c5140b91fc2b562
Tatu Honkanen <tatu.honkanen@cybercom.com>
2020-10-01 10:41:30 +0300

    OY-410 : Initial UrlConfiguration added.

---
+++ src/main/java/fi/oph/opintopolku/configurations/properties/UrlConfiguration.java
@@ -0,0 +1,17 @@
+package fi.oph.opintopolku.configurations.properties;
+
+import fi.vm.sade.properties.OphProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+@Configuration
+public class UrlConfiguration extends OphProperties {
+
+    @Autowired
+    public UrlConfiguration(Environment environment) {
+        addFiles("/oma-opintopolku-oph.properties");
+        addOverride("host-cas", environment.getRequiredProperty("host.host-cas"));
+        addOverride("host-oppija", environment.getRequiredProperty("host.host-oppija"));
+    }
+}

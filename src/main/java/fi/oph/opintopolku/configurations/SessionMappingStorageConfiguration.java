676bc02c23d14de0df90ef1278877983fa6d1c4e
Tatu Honkanen <tatu.honkanen@cybercom.com>
2020-10-01 16:05:17 +0300

    OY-410 : Added deps & create table sqls.

--- src/main/java/fi/oph/opintopolku/configurations/SessionMappingStorageConfiguration.java
+++ src/main/java/fi/oph/opintopolku/configurations/SessionMappingStorageConfiguration.java
@@ -5,13 +5,14 @@ import fi.oph.opintopolku.configurations.security.OphSessionMappingStorage;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.session.jdbc.JdbcIndexedSessionRepository;
 import org.springframework.session.jdbc.JdbcOperationsSessionRepository;
 
 @Configuration
 public class SessionMappingStorageConfiguration {
 
     @Bean
-    public OphSessionMappingStorage sessionMappingStorage(JdbcTemplate jdbcTemplate, JdbcOperationsSessionRepository sessionRepository) {
+    public OphSessionMappingStorage sessionMappingStorage(JdbcTemplate jdbcTemplate, JdbcIndexedSessionRepository sessionRepository) {
         return new JdbcSessionMappingStorage(jdbcTemplate, sessionRepository);
     }
 

7ce944cb40499f2424e1f5a5cd03a8f427e7c193
Tatu Honkanen <tatu.honkanen@cybercom.com>
2020-10-02 10:49:10 +0300

    OY-410 : Fix database name on pom.xml.

--- src/main/java/fi/oph/opintopolku/SessionController.java
+++ src/main/java/fi/oph/opintopolku/SessionController.java
@@ -7,8 +7,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
-
 
 @RestController
 @RequestMapping(value = "/session")

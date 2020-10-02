2263e4604d2e089966d33844a252045acb835e64
Tatu Honkanen <tatu.honkanen@cybercom.com>
2020-10-02 15:28:19 +0300

    OY-410 : Fix db request mapping.

--- src/main/java/fi/oph/opintopolku/SessionController.java
+++ src/main/java/fi/oph/opintopolku/SessionController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 
 @RestController
-@RequestMapping(value = "/session")
+@RequestMapping(value = "/initsession")
 public class SessionController {
     @PreAuthorize("#username == authentication.principal.username")
     @GetMapping

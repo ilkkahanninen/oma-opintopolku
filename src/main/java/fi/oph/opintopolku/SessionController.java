54f02cbbe25502abff15425184c9ef3275d76a94
Tatu Honkanen <tatu.honkanen@cybercom.com>
2020-10-14 16:17:48 +0300

    OY-410 : Cleanup, get user info from cas attributes

--- src/main/java/fi/oph/opintopolku/SessionController.java
+++ src/main/java/fi/oph/opintopolku/SessionController.java
@@ -3,6 +3,8 @@ package fi.oph.opintopolku;
 import lombok.val;
 import org.jasig.cas.client.authentication.AttributePrincipal;
 import org.joda.time.LocalDate;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.cas.authentication.CasAssertionAuthenticationToken;
 import org.springframework.security.cas.authentication.CasAuthenticationToken;
@@ -16,33 +18,32 @@ import java.util.Map;
 @RestController
 @RequestMapping(value = "/session")
 public class SessionController {
+    final static DateTimeFormatter formatter = DateTimeFormat.forPattern("ddMMYY");
     @PreAuthorize("isAuthenticated()")
     @GetMapping
 //    public User getSession(@RequestParam(value = "ticket", required = false) String ticket) {
     public User getSession() {
-
-        //private static Optional<Authentication> getAuthentication() {
-        //    return Optional.ofNullable(SecurityContextHolder.getContext().getAuthentication());
-        //}
-        // TODO test start
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         CasAuthenticationToken casAuthenticationToken = (CasAuthenticationToken) authentication;
         AttributePrincipal principal = casAuthenticationToken.getAssertion().getPrincipal();
         Map attributes = principal.getAttributes();
-        String personOid = (String) attributes.getOrDefault("personOid", "NOT_FOUND");
-        //TODO test end
 
-        //Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         val user = new User();
-        //val userName = getAuthentication().flatMap(authentication -> Optional.ofNullable(authentication.getName()));
-        user.setName("testi testiajaja");
-        //String displayName = ShibbolethUtils.parseDisplayName(etunimet, sukunimi);
-        //user.setName(displayName);
 
-       // LocalDate bd = ShibbolethUtils.parseDateFromHetu(hetu);
-       // user.setBirthDay(bd);
+        user.setName((String) attributes.getOrDefault("displayName", "NOT_FOUND"));
+
+        LocalDate bd = parseDateFromHetu((String) attributes.getOrDefault("nationalIdentificationNumber", "NOT_FOUND"));
+        user.setBirthDay(bd);
 
         return user;
     }
+
+    private static LocalDate parseDateFromHetu(String hetu) {
+        if (hetu != null) {
+            return formatter.parseLocalDate(hetu.substring(0, 6));
+        }
+        return null;
+    }
+
 
 }

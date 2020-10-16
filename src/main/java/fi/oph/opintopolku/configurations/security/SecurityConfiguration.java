0edb1156888ff9f47201d9634d202352c12c572b
Tatu Honkanen <tatu.honkanen@cybercom.com>
2020-10-16 15:29:06 +0300

    OY-410 : Get hos from environment

--- src/main/java/fi/oph/opintopolku/configurations/security/SecurityConfiguration.java
+++ src/main/java/fi/oph/opintopolku/configurations/security/SecurityConfiguration.java
@@ -46,7 +46,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Bean
     public ServiceProperties serviceProperties() {
         ServiceProperties serviceProperties = new ServiceProperties();
-        serviceProperties.setService(casOppijaProperties.getService() + "/j_spring_cas_security_check");
+        serviceProperties.setService(environment.getRequiredProperty("host.host-oppija") + "/j_spring_cas_security_check");
         serviceProperties.setSendRenew(casOppijaProperties.getSendRenew());
         serviceProperties.setAuthenticateAllArtifacts(true);
         return serviceProperties;

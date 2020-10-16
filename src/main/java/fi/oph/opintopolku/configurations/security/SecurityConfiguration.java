e8c251b822b0575472a3f750d8a0dfbd01743a79
Tatu Honkanen <tatu.honkanen@cybercom.com>
2020-10-16 13:30:22 +0300

    OY-410 : Switch sso-filter

--- src/main/java/fi/oph/opintopolku/configurations/security/SecurityConfiguration.java
+++ src/main/java/fi/oph/opintopolku/configurations/security/SecurityConfiguration.java
@@ -92,7 +92,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Bean
     public SingleSignOutFilter singleSignOutFilter() {
         SingleSignOutFilter singleSignOutFilter = new SingleSignOutFilter();
-        singleSignOutFilter.setCasServerUrlPrefix(this.ophProperties.url("url-cas"));
+        singleSignOutFilter.setCasServerUrlPrefix(this.ophProperties.url("cas-oppija.url"));
         singleSignOutFilter.setIgnoreInitConfiguration(true);
         singleSignOutFilter.setSessionMappingStorage(sessionMappingStorage);
         return singleSignOutFilter;

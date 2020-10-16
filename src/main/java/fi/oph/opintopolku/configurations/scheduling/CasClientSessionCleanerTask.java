a853f5c641c4b19656e333a54c6f838f50daacf5
Tatu Honkanen <tatu.honkanen@cybercom.com>
2020-10-16 13:24:07 +0300

    OY-410 : Client session cleanup added.

---
+++ src/main/java/fi/oph/opintopolku/configurations/scheduling/CasClientSessionCleanerTask.java
@@ -0,0 +1,29 @@
+package fi.oph.opintopolku.configurations.scheduling;
+
+import com.github.kagkarlsson.scheduler.task.ExecutionContext;
+import com.github.kagkarlsson.scheduler.task.TaskInstance;
+import com.github.kagkarlsson.scheduler.task.helper.RecurringTask;
+import com.github.kagkarlsson.scheduler.task.schedule.FixedDelay;
+
+import fi.oph.opintopolku.configurations.security.OphSessionMappingStorage;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+
+@Component
+public class CasClientSessionCleanerTask extends RecurringTask {
+
+    private final OphSessionMappingStorage sessionMappingStorage;
+
+    public CasClientSessionCleanerTask(OphSessionMappingStorage sessionMappingStorage) {
+        super("cas client session cleaner", FixedDelay.of(Duration.ofHours(1)), Void.class, null);
+        this.sessionMappingStorage = sessionMappingStorage;
+    }
+
+    @Override
+    public void executeRecurringly(TaskInstance taskInstance, ExecutionContext executionContext) {
+        sessionMappingStorage.clean();
+    }
+
+}
+

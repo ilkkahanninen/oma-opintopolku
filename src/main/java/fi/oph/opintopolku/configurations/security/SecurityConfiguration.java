93f5b407f7706a0259648e211cc8c548de687343
Tatu Honkanen <tatu.honkanen@cybercom.com>
2020-10-16 08:17:29 +0300

    OY-410 : test service url

--- src/main/java/fi/oph/opintopolku/configurations/security/SecurityConfiguration.java
+++ src/main/java/fi/oph/opintopolku/configurations/security/SecurityConfiguration.java
@@ -44,7 +44,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Bean
     public ServiceProperties serviceProperties() {
         ServiceProperties serviceProperties = new ServiceProperties();
-        serviceProperties.setService(casOppijaProperties.getService());
+        serviceProperties.setService(casOppijaProperties.getService() + "/j_spring_cas_security_check");
         serviceProperties.setSendRenew(casOppijaProperties.getSendRenew());
         serviceProperties.setAuthenticateAllArtifacts(true);
         return serviceProperties;

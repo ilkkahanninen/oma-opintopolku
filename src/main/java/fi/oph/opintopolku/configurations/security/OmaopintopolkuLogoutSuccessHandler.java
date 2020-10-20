01061c2353bd3fc7ced783c67229e381502fd20b
Tatu Honkanen <tatu.honkanen@cybercom.com>
2020-10-20 13:56:10 +0300

    OY-410 : WIP - call super on logout

--- src/main/java/fi/oph/opintopolku/configurations/security/OmaopintopolkuLogoutSuccessHandler.java
+++ src/main/java/fi/oph/opintopolku/configurations/security/OmaopintopolkuLogoutSuccessHandler.java
@@ -2,17 +2,20 @@ package fi.oph.opintopolku.configurations.security;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import org.springframework.security.web.authentication.logout.SimpleUrlLogoutSuccessHandler;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class OmaopintopolkuLogoutSuccessHandler implements LogoutSuccessHandler {
+public class OmaopintopolkuLogoutSuccessHandler extends
+    SimpleUrlLogoutSuccessHandler implements LogoutSuccessHandler {
 
     @Override
     public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response,
-                                Authentication authentication) throws IOException {
+                                Authentication authentication) throws IOException, ServletException {
+        super.onLogoutSuccess(request, response, authentication);
         response.sendRedirect(request.getParameter("service"));
     }
 }

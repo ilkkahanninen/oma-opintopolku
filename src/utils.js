9ea5e0156cdeeaad430a5e189a3cbbbd8fb6214f
Tatu Honkanen <tatu.honkanen@cybercom.com>
2020-10-27 15:21:03 +0200

    OY-410 : Add domain to session fetch

--- src/utils.js
+++ src/utils.js
@@ -8,7 +8,10 @@ const domains = {
 
 export function getUser() {
   return new Promise((resolve, reject) => {
-    fetch('/oma-opintopolku/session', {
+    const lang = getLang().toUpperCase();
+    const domain = createDomain(lang);
+    const sessionUrl = domain + '/oma-opintopolku/session'
+      fetch(sessionUrl, {
       headers: new Headers({'Caller-Id': '1.2.246.562.10.00000000001.oma-opintopolku.frontend'}),
       credentials: 'same-origin'
     })

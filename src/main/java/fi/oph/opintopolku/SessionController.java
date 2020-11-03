0c7dfb400398ed7057f1bf44e6ef9646a68a7fcd
Tatu Honkanen <tatu.honkanen@cybercom.com>
2020-11-03 11:45:54 +0200

    OY-410 : Corsfilter to HttpSecurity

--- src/main/java/fi/oph/opintopolku/SessionController.java
+++ src/main/java/fi/oph/opintopolku/SessionController.java
@@ -9,10 +9,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.cas.authentication.CasAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
 
 import java.text.DateFormat;

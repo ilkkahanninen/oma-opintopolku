4b1a189a966eb241ee677376b559a6a3d0566f8a
Tatu Honkanen <tatu.honkanen@cybercom.com>
2020-12-16 12:17:01 +0200

    OY-416 : Set service url to casAuthenticationFilter

--- src/main/java/fi/oph/opintopolku/configurations/security/SecurityConfiguration.java
+++ src/main/java/fi/oph/opintopolku/configurations/security/SecurityConfiguration.java
@@ -82,7 +82,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     public CasAuthenticationFilter casAuthenticationFilter() throws Exception {
         OpintopolkuCasAuthenticationFilter casAuthenticationFilter = new OpintopolkuCasAuthenticationFilter(serviceProperties());
         casAuthenticationFilter.setAuthenticationManager(authenticationManager());
-        casAuthenticationFilter.setFilterProcessesUrl("/j_spring_cas_security_check");
+        casAuthenticationFilter.setFilterProcessesUrl(casOppijaProperties.getService() + "/j_spring_cas_security_check");
         return casAuthenticationFilter;
     }
 
